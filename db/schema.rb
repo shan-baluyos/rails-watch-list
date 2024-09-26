bc9ff6430170835c06d3ae78993de513c1db329a
Shan Baluyos <shan.baluyos21@gmail.com>
2024-09-26 16:07:01 +0100

    added movie model

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_26_145007) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.text "overview"
+    t.text "poster_url"
+    t.decimal "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
